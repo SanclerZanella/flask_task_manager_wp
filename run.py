b115b2cb5e7eca5fe8ebe478770a0505cfc2f7bd
Sancler Zanella <sanclerzjj@gmail.com>
2021-06-18 14:26:06 +0000

    Bind data from db to accordion

--- run.py
+++ run.py
@@ -106,7 +106,7 @@ def logout():
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find()
+    tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
 
 
